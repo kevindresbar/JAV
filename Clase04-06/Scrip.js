a50449957c60bf37d2fb84ca3e74c8b609e63d19
Kevin <kevindresbar@gmail.com>
2026-04-06 19:24:57 -0500

    push y pop

---
+++ Clase04-06/Scrip.js
@@ -0,0 +1,18 @@
+// datos de referencia
+// arrays es una lista de componentes
+// ejemplo let notas =[5, 4, 99,] se separan por coma ,
+//length -1 para el ultimo 
+
+//dos metodos push y pop 
+ //push agrega al ultimo un elemento y el poc eliminael ultimo elemento 
+
+ let productos= []
+
+ let product1 = prompt("ingrese el primer producto")
+   productos.push ("product1")
+
+     let product2 = prompt("ingrese el segundo producto")
+     productos.push ("product2")
+   let product3 = prompt("ingrese el tercer producto")
+productos.push ("product3")
+console.log("tus prouctos son", productos)
