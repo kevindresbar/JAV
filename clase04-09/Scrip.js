e18ea7a88cae37f0ad0f8b7115e31294c3585770
Kevin <kevindresbar@gmail.com>
2026-04-09 19:44:51 -0500

    1 a 100

--- clase04-09/Scrip.js
+++ clase04-09/Scrip.js
@@ -1,20 +1,28 @@
 //nuevo condiicional un switvh es en caso es mas sencillo y no se usan normalmente condiciones 
-let azar = Math.floor((Math.random()*10)+1)
+let azar = Math.floor((Math.random()*100)+1)
 intentos = 0
-maxintentos = 6
+maxintentos = 11
 
 while (intentos < maxintentos){
-    intento = prompt("Adivina el número entre 1 y 10: ")
+    intento = prompt("Adivina el número entre 1 y 100: ")
     intentos += 1
 
     if (intento == azar){
-         console.log("Correcto adivinaste el número ")
+         alert("Correcto adivinaste el número ")
         break}
-    else{
+        else if(intento < azar){
+             alert("el numero es mayor intente otra vez ")
+        }
+        else if(intento > azar){
+             alert("el numero es menor intente otra vez ")
+
+        }
+        else{
          console.log("Incorrecto")
          
         }
-        
-}
-if (intento != azar){
- console.log("Perdiste el número era ", azar)}
+        if ( intento != azar){
+  console.log("Perdiste el número era ", azar)}
+
+    }       
+            
